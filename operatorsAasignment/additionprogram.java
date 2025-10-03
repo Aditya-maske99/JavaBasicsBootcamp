73755023e7301195d7340a0f93fe9457983d31a3
Aditya Maske <maskeaditya764@gmail.com>
2025-10-03 12:01:32 +0530

    Addition

---
+++ operatorsAasignment/additionprogram.java
@@ -0,0 +1,11 @@
+package operatorsAasignment;
+
+public class additionprogram {
+    public static void main(String[] args) {
+        int a=5;
+        int b=5;
+        int c =a+b;
+        System.out.print("Addition of Two numbers :"+c);
+    }
+
+}
