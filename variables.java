6a44cf5499eac5dc5be0bfc1a96ffcd34fe43e92
Aditya Maske <maskeaditya7642gmail.com>
2025-10-04 22:49:51 +0530

    variables

---
+++ variables.java
@@ -0,0 +1,9 @@
+public class variables{
+    public static void main(String[] args) {
+        int x = 5;
+       double y = 5.2; 
+        String z = "Aditya";
+
+        System.out.println(x + ", " + y + ", " + z);
+    }
+}
